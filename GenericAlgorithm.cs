d5b0c73b3bbde5a3008b70d1bd439d545407e528
LunaWhei <47643516+LunaWhei@users.noreply.github.com>
2021-04-23 08:55:15 +0200

    Add files via upload

---
+++ GenericAlgorithm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace genetykav2
+{
+	public class GeneticAlgorithm<T>
+	{
+		public List<DNA<T>> Population { get; private set; }
+		public int Generation { get; private set; }
+		public float BestFitness { get; private set; }
+		public T[] BestGenes { get; private set; }
+
+		public int Elitism;
+		public float MutationRate;
+
+		private List<DNA<T>> newPopulation;
+		private Random random;
+		private float fitnessSum;
+		private int dnaSize;
+		private Func<T> getRandomGene;
+		private Func<int, float> fitnessFunction;
+        private int populationSize;
+        private int length;
+        private Func<char> getrandomGene;
+        private float fitnessFunction1;
+        private int v;
+
+        public GeneticAlgorithm(int populationSize, int dnaSize, Random random, Func<T> getRandomGene, Func<int, float> fitnessFunction,
+			int elitism, float mutationRate = 0.01f)
+		{
+			Generation = 1;
+			Elitism = elitism;
+			MutationRate = mutationRate;
+			Population = new List<DNA<T>>(populationSize);
+			newPopulation = new List<DNA<T>>(populationSize);
+			this.random = random;
+			this.dnaSize = dnaSize;
+			this.getRandomGene = getRandomGene;
+			this.fitnessFunction = fitnessFunction;
+
+			BestGenes = new T[dnaSize];
+
+			for (int i = 0; i < populationSize; i++)
+			{
+				Population.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
+			}
+		}
+
+        public GeneticAlgorithm(int populationSize, int length, Random random, Func<char> getrandomGene, float fitnessFunction, int v, float mutationRate)
+        {
+            this.populationSize = populationSize;
+            this.length = length;
+            this.random = random;
+            this.getrandomGene = getrandomGene;
+            fitnessFunction1 = fitnessFunction;
+            this.v = v;
+            MutationRate = mutationRate;
+        }
+
+        public void NewGeneration(int numNewDNA = 0, bool crossoverNewDNA = false)
+		{
+			int finalCount = Population.Count + numNewDNA;
+
+			if (finalCount <= 0)
+			{
+				return;
+			}
+
+			if (Population.Count > 0)
+			{
+				CalculateFitness();
+				Population.Sort(CompareDNA);
+			}
+			newPopulation.Clear();
+
+			for (int i = 0; i < Population.Count; i++)
+			{
+				if (i < Elitism && i < Population.Count)
+				{
+					newPopulation.Add(Population[i]);
+				}
+				else if (i < Population.Count || crossoverNewDNA)
+				{
+					DNA<T> parent1 = ChooseParent();
+					DNA<T> parent2 = ChooseParent();
+
+					DNA<T> child = parent1.Crossover(parent2);
+
+					child.Mutate(MutationRate);
+
+					newPopulation.Add(child);
+				}
+				else
+				{
+					newPopulation.Add(new DNA<T>(dnaSize, random, getRandomGene, fitnessFunction, shouldInitGenes: true));
+				}
+			}
+
+			List<DNA<T>> tmpList = Population;
+			Population = newPopulation;
+			newPopulation = tmpList;
+
+			Generation++;
+		}
+
+		private int CompareDNA(DNA<T> a, DNA<T> b)
+		{
+			if (a.Fitness > b.Fitness)
+			{
+				return -1;
+			}
+			else if (a.Fitness < b.Fitness)
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+
+		private void CalculateFitness()
+		{
+			fitnessSum = 0;
+			DNA<T> best = Population[0];
+
+			for (int i = 0; i < Population.Count; i++)
+			{
+				fitnessSum += Population[i].CalculateFitness(i);
+
+				if (Population[i].Fitness > best.Fitness)
+				{
+					best = Population[i];
+				}
+			}
+
+			BestFitness = best.Fitness;
+			best.Genes.CopyTo(BestGenes, 0);
+		}
+
+		private DNA<T> ChooseParent()
+		{
+		//	double randomNumber = random.NextDouble() * fitnessSum;
+
+			int randomParent1 = random.Next(0, Population.Count);
+			int randomParent2 = random.Next(0, Population.Count);
+			
+
+                if (Population[randomParent1].Fitness > Population[randomParent2].Fitness)
+            {
+				return Population[randomParent1];
+            }
+            else
+            {
+				return Population[randomParent2];
+            }
+			//for (int i = 0; i < Population.Count; i++)
+			//{
+			//	if (randomNumber < Population[i].Fitness)
+			//	{
+			//		return Population[i];
+			//	}
+
+			//	randomNumber -= Population[i].Fitness;
+			//}
+
+			//return null;
+		}
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+
+namespace genetykav2
+{
+    
+    class Program
+    {
+       static DateTime localDate = DateTime.Now;
+        static GeneticAlgorithm<char> ga;
+        static void Main(string[] args)
+        {
+            FileStream fileStream = File.Open("log.txt", FileMode.Open);
+            fileStream.SetLength(0);
+            fileStream.Close(); 
+
+                Random random = new Random();
+            
+            int populationSize = 200;
+            float mutationRate = 0.01f;
+            int elitism = 1;
+            string targetString = "Sample string to be replaced";
+            string validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ,.|!@#$%^&*()_+=? ";
+
+            Console.WriteLine("podaj rozmiar populacji");
+            populationSize =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Podaj szanse na mutacje eg.: 0.03");
+            mutationRate = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj idealne geny końcowe w formie teksowej eg: To be or not to be!");
+            Console.WriteLine("Zdanie nie może zawierać polskich znaków");
+            targetString = Console.ReadLine();
+
+
+
+            Console.WriteLine("Podaj poziom elitismu (zakres od 0 do 100)");
+            elitism = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine("Podaj liczbę iteracji, 0 jeżeli ma dążyć do pokolenia z najlepszymi możliwymi genami");
+            int howlong = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.WriteLine("Algorytm wykonany: {0} Rozmiar populacji: {1} szansa na mutacje: {2}% elitism: {3}% Geny docelowe: {4}", 
+                    localDate.ToString(),
+                    populationSize.ToString(),
+                    mutationRate.ToString(),
+                    elitism.ToString(),
+                    targetString.ToString()
+                    
+                    );
+                w.Close();
+            }
+            ga = new GeneticAlgorithm<char>(
+                populationSize,
+                targetString.Length,
+                random,
+                GetrandomGene,
+                fitnessFunction: FitnessFunction,
+                elitism,
+                mutationRate: mutationRate);
+            string text="";
+            if (howlong == 0)
+            {
+                while (text != targetString)
+                {
+                    ga.NewGeneration();
+                    Console.WriteLine(ga.Generation);
+                    Console.WriteLine(ga.BestGenes);
+                     text = new string(ga.BestGenes);
+                    using (StreamWriter w = File.AppendText("log.txt"))
+                    {
+                        w.WriteLine("{0} Najlepsze geny w populacji: {1} Fitness:{2}",
+                           ga.Generation.ToString(),
+                           text,
+                           ga.BestFitness);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < howlong; i++)
+                {
+                    ga.NewGeneration();
+                    Console.WriteLine(ga.Generation);
+                    Console.WriteLine(ga.BestGenes);
+                    text = new string(ga.BestGenes);
+                    using (StreamWriter w = File.AppendText("log.txt"))
+                    {
+                        w.WriteLine("{0} Najlepsze geny w populacji: {1} Fitness:{2}",
+                           ga.Generation.ToString(),
+                           text,
+                           ga.BestFitness);
+                    }
+                }
+            }
+            
+
+
+             char GetrandomGene()
+            {
+               
+                int i = random.Next(validCharacters.Length);
+                return validCharacters[i];
+            }
+
+            float FitnessFunction(int index)
+            {
+                float score = 0;
+                DNA<char> DNA;
+                DNA = ga.Population[index];
+                for (int i = 0; i < DNA.Genes.Length; i++)
+                {
+                    if (DNA.Genes[i] == targetString[i])
+                    {
+                        score += 1;
+                    }
+                }
+                score /= targetString.Length;
+                return score;
+            }
+        }
+
+        
+    }
+
+
+    public class DNA<T>
+    {
+        public T[] Genes { get; private set; }
+        public float Fitness { get; private set; }
+
+        private Random random;
+        private Func<T> getRandomGene;
+        private Func<int, float> fitnessFunction;
+
+        public DNA(int size, Random random, Func<T> getRandomGene, Func<int, float> fitnessFunction, bool shouldInitGenes = true)
+        {
+            Genes = new T[size];
+            this.random = random;
+            this.getRandomGene = getRandomGene;
+            this.fitnessFunction = fitnessFunction;
+
+            if (shouldInitGenes)
+            {
+                for (int i = 0; i < Genes.Length; i++)
+                {
+                    Genes[i] = getRandomGene();
+                }
+            }
+        }
+
+        public float CalculateFitness(int index)
+        {
+            Fitness = fitnessFunction(index);
+            return Fitness;
+        }
+
+        public DNA<T> Crossover(DNA<T> otherParent)
+        {
+            DNA<T> child = new DNA<T>(Genes.Length, random, getRandomGene, fitnessFunction, shouldInitGenes: false);
+
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                child.Genes[i] = random.NextDouble() < 0.5 ? Genes[i] : otherParent.Genes[i];
+            }
+
+            return child;
+        }
+
+        public void Mutate(float mutationRate)
+        {
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                if (random.NextDouble() < mutationRate)
+                {
+                    Genes[i] = getRandomGene();
+                }
+            }
+        }
+    }
+
+
+}
